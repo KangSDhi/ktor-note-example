ee59f4c909639f57c6da85f3b5c60735f12df939
KangSDhi <kangteknisi@gmail.com>
2023-01-17 20:25:12 +0700

    Ktor Project Structure and basic Routing

--- src/main/kotlin/com/sigit/Application.kt
+++ src/main/kotlin/com/sigit/Application.kt
@@ -4,12 +4,20 @@ import io.ktor.server.application.*
 import io.ktor.server.engine.*
 import io.ktor.server.netty.*
 import com.sigit.plugins.*
+import io.ktor.server.application.*
+import io.ktor.server.response.*
+import io.ktor.server.routing.*
 
-fun main() {
-    embeddedServer(Netty, port = 8080, host = "0.0.0.0", module = Application::module)
-        .start(wait = true)
+fun main(){
+    embeddedServer(Netty, port=8080, host="0.0.0.0"){
+        install(Routing){
+            homeRoute()
+        }
+    }.start(wait = true)
 }
 
-fun Application.module() {
-    configureRouting()
+fun Routing.homeRoute(){
+    get("/") {
+        call.respond("Hello Ktor.")
+    }
 }
