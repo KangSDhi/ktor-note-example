13455cda2db7d0e8b7f28b0329cf80b755d952e7
KangSDhi <kangteknisi@gmail.com>
2023-01-17 20:32:57 +0700

    Ktor Module based routing

--- src/main/kotlin/com/sigit/Application.kt
+++ src/main/kotlin/com/sigit/Application.kt
@@ -10,14 +10,9 @@ import io.ktor.server.routing.*
 
 fun main(){
     embeddedServer(Netty, port=8080, host="0.0.0.0"){
-        install(Routing){
-            homeRoute()
-        }
+        configureRouting()
+        contactUsModule()
     }.start(wait = true)
 }
 
-fun Routing.homeRoute(){
-    get("/") {
-        call.respond("Hello Ktor.")
-    }
-}
+
