0cefa68b3200915571f4c08710c6f7c7d311ed0f
KangSDhi <kangteknisi@gmail.com>
2023-01-19 08:35:45 +0700

    Ktor Response - Sending JSON Response

--- src/main/kotlin/com/sigit/plugins/Routing.kt
+++ src/main/kotlin/com/sigit/plugins/Routing.kt
@@ -1,9 +1,11 @@
 package com.sigit.plugins
 
+import io.ktor.http.*
 import io.ktor.server.routing.*
 import io.ktor.server.response.*
 import io.ktor.server.application.*
 import io.ktor.server.request.*
+import io.ktor.util.reflect.*
 import kotlinx.serialization.*
 
 fun Application.configureRouting() {
@@ -23,8 +25,19 @@ fun Application.configureRouting() {
             print(userInfo)
             call.respondText("Everything working.")
         }
+
+        get("/error"){
+            call.respondText("Something went wrong", status = HttpStatusCode.GatewayTimeout)
+        }
+
+        get("/user"){
+            val responseObject = UserResponse("sigit", "kangsigit@gmail.com")
+            call.respond(responseObject)
+        }
     }
 }
 
 @Serializable
 data class UserInfo(val email: String, val password: String)
+@Serializable
+data class UserResponse(val name: String, val email: String)
